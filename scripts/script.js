40d05357aa3e6fe9fbf34f57365bd082ab1eaf4c
CathelijneVisser <112855711+CathelijneVisser@users.noreply.github.com>
2023-01-17 16:25:11 +0100

    wip
    intro
    
    footer

--- scripts/script.js
+++ scripts/script.js
@@ -1,12 +1,23 @@
 const DescriptionButton = document.querySelector(".more-description")
 const DescriptionButtonLess = document.querySelector(".less")
 const Description = document.querySelectorAll(".more-info-description")
+const FooterServices = document.querySelector(".services")
+const FooterInformation = document.querySelector(".information")
+const FooterRealetor = document.querySelector(".realetor-footer")
+const FooterAboutUs = document.querySelector(".about-us")
+const FooterSVG = document.querySelectorAll("footer svg")
 
-DescriptionButton.addEventListener("click", DescriptionExpand)
-DescriptionButtonLess.addEventListener("click", DescriptionExpand)
+// DescriptionButton.addEventListener("click", DescriptionExpand)
+// DescriptionButtonLess.addEventListener("click", DescriptionExpand)
 
-function DescriptionExpand () {
-    Description.forEach.classList.toggle("fold-out")
-    DescriptionButton.classList.toggle("expanded")
-    DescriptionButtonLess.classList.toggle("expanded")
-}
+// function DescriptionExpand () {
+//     Description.forEach.classList.toggle("fold-out")
+//     DescriptionButton.classList.toggle("expanded")
+//     DescriptionButtonLess.classList.toggle("expanded")
+// }
+
+FooterSVG.forEach((svg) => {
+    svg.addEventListener("click", () => {
+        svg.parentElement.classList.toggle("expand")
+    })
+})
