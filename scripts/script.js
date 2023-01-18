c95735836cb8dc21fd27467c63ad87d6546e12bc
CathelijneVisser <112855711+CathelijneVisser@users.noreply.github.com>
2023-01-18 15:38:11 +0100

    wip
    2de knop gemaakt

--- scripts/script.js
+++ scripts/script.js
@@ -1,23 +1,40 @@
-const DescriptionButton = document.querySelector(".more-description")
-const DescriptionButtonLess = document.querySelector(".less")
-const Description = document.querySelectorAll(".more-info-description")
-const FooterServices = document.querySelector(".services")
-const FooterInformation = document.querySelector(".information")
-const FooterRealetor = document.querySelector(".realetor-footer")
-const FooterAboutUs = document.querySelector(".about-us")
-const FooterSVG = document.querySelectorAll("footer svg")
+const DescriptionButton = document.querySelector(".more-description");
+const DescriptionButtonLess = document.querySelector(".less");
+const Description = document.querySelectorAll(".more-info-description");
+const FooterServices = document.querySelector(".services");
+const FooterInformation = document.querySelector(".information");
+const FooterRealetor = document.querySelector(".realetor-footer");
+const FooterAboutUs = document.querySelector(".about-us");
+const FooterSVG = document.querySelectorAll("footer svg");
+const CharacteristicsButton = document.querySelector(".more-characteristics");
+const CharacteristicsButtonLess = document.querySelector(
+	".less-characteristics"
+);
+const characteristics = document.querySelector(".characteristics");
 
-// DescriptionButton.addEventListener("click", DescriptionExpand)
-// DescriptionButtonLess.addEventListener("click", DescriptionExpand)
+DescriptionButton.addEventListener("click", DescriptionExpand);
+DescriptionButtonLess.addEventListener("click", DescriptionExpand);
+CharacteristicsButton.addEventListener("click", CharacteristicsExpand);
+CharacteristicsButtonLess.addEventListener("click", CharacteristicsExpand);
 
-// function DescriptionExpand () {
-//     Description.forEach.classList.toggle("fold-out")
-//     DescriptionButton.classList.toggle("expanded")
-//     DescriptionButtonLess.classList.toggle("expanded")
-// }
+function DescriptionExpand() {
+	Description.forEach((Description) => {
+		Description.classList.toggle("fold-out");
+	});
+	DescriptionButton.classList.toggle("expanded");
+	DescriptionButtonLess.classList.toggle("expanded");
+}
+
+function CharacteristicsExpand() {
+	Characteristics.forEach((Characteristics) => {
+		Characteristics.classList.toggle("fold-out");
+	});
+	Characteristics.classList.toggle("expanded");
+	CharacteristicsButtonLess.classList.toggle("expanded");
+}
 
 FooterSVG.forEach((svg) => {
-    svg.addEventListener("click", () => {
-        svg.parentElement.classList.toggle("expand")
-    })
-})
+	svg.addEventListener("click", () => {
+		svg.parentElement.classList.toggle("expand");
+	});
+});
