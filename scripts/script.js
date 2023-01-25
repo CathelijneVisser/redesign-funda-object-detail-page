a3402a726aafbcc1655c512ae230537367686848
CathelijneVisser <112855711+CathelijneVisser@users.noreply.github.com>
2023-01-25 12:19:54 +0100

    carrousel pijltjes

--- scripts/script.js
+++ scripts/script.js
@@ -7,12 +7,14 @@ const characteristicsButton = document.querySelector(".more-characteristics");
 const characteristicsButtonLess = document.querySelector(".less-characteristics");
 const characteristics = document.querySelector(".more-info-characteristics");
 const footerFoldouts = document.querySelectorAll(".fold-out");
+const carrousel = document.querySelector('div.carrousel');
 
 menuButton.addEventListener("click", menuExpand);
 descriptionButton.addEventListener("click", descriptionExpand);
 descriptionButtonLess.addEventListener("click", descriptionExpand);
 characteristicsButton.addEventListener("click", characteristicsExpand);
 characteristicsButtonLess.addEventListener("click", characteristicsExpand);
+carrousel.addEventListener('click', scrollLeftOrRight);
 
 function menuExpand () {
 	nav.classList.toggle("expanded");
@@ -37,3 +39,28 @@ footerFoldouts.forEach((foldOut) => {
 		foldOut.parentElement.classList.toggle('active')
 	})
 })
+
+
+function scrollLeftOrRight(uiEvent) {
+
+ 
+ // Niet heel relevant, maar hiermee zorg je dat de afmetingen van de carrousel in CSS blijven
+ // En pas in JavaScript worden opgevraagd
+ var scrollWidth = this.scrollWidth;
+ var offsetWidth = this.offsetWidth;
+ var scrollLeft = this.scrollLeft;
+ // Scroll naar links, of naar rechts, afhankelijk van waar we klikten
+ var scrollXBy = (uiEvent.offsetX < offsetWidth / 2 ? -1 : 1) * offsetWidth;
+ if (scrollXBy < 0 && scrollLeft == 0) {
+  // Als we bij de eerste afbeelding zijn, ga dan naar de laatste
+  scrollXBy = scrollWidth - offsetWidth;
+ } else if (scrollXBy > 0 && Math.abs(scrollWidth - (scrollLeft + offsetWidth)) <= 1) {
+  // Als we bij de laatste afbeelding zijn, ga dan naar de eerste
+  scrollXBy = (-1 * scrollWidth) + scrollXBy;
+ }
+ this.scrollBy({
+  left: scrollXBy
+ });
+
+
+}
